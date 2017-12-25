63730b7c3458c6fa64ca3814691076bbfb6a6dc2
Jonathan Templin <jtemplin@ku.edu>
2017-12-25 12:52:40 -0600

    Working Package

---
+++ R/data.R
@@ -0,0 +1,89 @@
+#' Data file for examples used in EPSY 905
+#'
+#' A dataset containing the heights, weights, and sex of 20 hypothetical people
+#'
+#'
+#' @format A data frame with 20 observations and 4 variables
+#' \describe{
+#'   \item{id}{id number}
+#'   \item{sex}{sex of person}
+#'   \item{heightIN}{height in inches}
+#'   \item{weightLB}{weight in pounds}
+#' }
+"dataSexHeightWeight"
+
+#' Data file for examples used in EPSY 905
+#'
+#' A dataset containing the data for interactions examples
+#'
+#'
+#' @format A data frame with 100 observations and 5 variables
+#' \describe{
+#'   \item{PersonID}{Id number}
+#'   \item{Senior}{Dummy-coded indicator of a person's status as a Senior (vs. Freshman)}
+#'   \item{New}{Dummy-coded indicator of a person's status as being in the New type of instruction group (vs. Old)}
+#'   \item{Group}{Numeric gruop number produced from permutation of Senior and New (four groups total)}
+#'   \item{Test}{Test score}
+#' }
+"dataTestExperiment"
+
+#' Data file for examples used in EPSY 905
+#'
+#' A dataset containing five observations used to show likelihoods
+#'
+#'
+#' @format A data frame with 5 observations and 3 variables
+#' \describe{
+#'   \item{id}{id number}
+#'   \item{iq}{IQ score}
+#'   \item{Perf}{Job performance score}
+#' }
+"dataPerfIQ"
+
+#' Data file for examples used in EPSY 905
+#'
+#' A dataset containing SAT Math and Verbal Scores (simulated)
+#'
+#'
+#' @format A data frame with 5 observations and 3 variables
+#' \describe{
+#'   \item{id}{id number}
+#'   \item{SATV}{SAT Verbal score}
+#'   \item{SATM}{SAT Math score}
+#' }
+"dataSAT"
+
+#' Data file for examples used in EPSY 905
+#'
+#' A dataset containing mathematics efficacy study data (simulated)
+#'
+#'
+#' @format A data frame with 350 observations and 9 variables
+#' \describe{
+#'   \item{id}{id number}
+#'   \item{hsl}{Mathematics courses taken in high school}
+#'   \item{cc}{Credit hours in mathematics courses in college}
+#'   \item{use}{Perceived usefulness of mathematics score}
+#'   \item{msc}{Mathematics self concept score}
+#'   \item{mas}{Mathematics anxiety score}
+#'   \item{mse}{Mathematics self efficacy score}
+#'   \item{perf}{Performance on mathematics test (number correct out of 20 items)}
+#'   \item{female}{Dummy coded sex where female = 1}
+#' }
+"dataMath"
+
+#' Data file for examples used in EPSY 905
+#'
+#' A dataset containing repeated measures of heartrates from an exercise study
+#'
+#'
+#' @format A data frame with 18 observations and 6 variables
+#' \describe{
+#'   \item{exertype}{Type of exercise (experimental group; 1=stair climbing; 2=Racquetball; 3=Weight training)}
+#'   \item{pulse1}{Pulse rate after warm up}
+#'   \item{pulse2}{Pulse rate after jogging}
+#'   \item{pulse3}{Pulse rate after running}
+#'   \item{diet}{diet group (1= vegetarians; 2 = omnivores)}
+#'   \item{id}{id number}
+#' }
+"dataExercise"
---
+++ R/plot_bvn_surface.R
@@ -0,0 +1,38 @@
+
+#' A function that plots the surface of a bivariate normal distribution
+#'
+#' @param meanvec A matrix with 2 rows andone column that stores the mean of the bivariate normal distribution
+#' @param covmat A 2 by 2 symmetric covariance matrix
+#' @param type A character stating the type of plot from the plot.surface function of the fields package ("c" is a contour, "p" is a perspective/drape plot)
+#' @param xlab A character for the x-axis label
+#' @param ylab A character for the y-axis label
+#' @param zlab A character for the z-axis label
+#' @param main A character for the main label
+#' @return A plot of the surface of a bivariate normal with meanvec as the mean vector and covmat as the covariance matrix
+#' @examples
+#' plot_bvn_surface(meanvec = matrix(c(0,0), nrow = 2, ncol = 1),
+#'                  covmat = diag(2), type = "c", xlab = "x",
+#'                  ylab = "y", zlab = "f(x,y)", main = "contour plot")
+#' plot_bvn_surface(meanvec = matrix(c(0,0), nrow = 2, ncol = 1),
+#'                  covmat = diag(2), type = "p", xlab = "x",
+#'                  ylab = "y", zlab = "f(x,y)", main = "drape plot")
+plot_bvn_surface = function(meanvec, covmat, type, xlab, ylab, zlab, main){
+
+  #creating values for x and y axes based on estimated values from model
+  x = matrix(seq(meanvec[1,1]-4*sqrt(covmat[1,1]), meanvec[1,1]+4*sqrt(covmat[1,1]), .1*sqrt(covmat[1,1])), ncol=1)
+  y = matrix(seq(meanvec[2,1]-4*sqrt(covmat[2,2]), meanvec[2,1]+4*sqrt(covmat[2,2]), .1*sqrt(covmat[2,2])), ncol=1)
+  z = matrix(0,nrow = dim(x)[1],ncol = dim(y)[1])
+
+  for (i in 1:dim(x)[1]){
+    for (j in 1:dim(y)[1]){
+      z[i,j] = mnormt::dmnorm(c(x[i,1],y[j,1]),mean = t(meanvec), varcov = covmat, log = FALSE)
+    }
+  }
+
+  grid.list=list(x = x, y = y)
+  z1 = z[1:dim(x)[1]-1,1:dim(y)[1]-1]
+  mygrid = fields::make.surface.grid(grid.list)
+  out = list(x = grid.list$x, y = grid.list$y, z = z)
+
+  fields::plot.surface(out,type=type, xlab=xlab, ylab=ylab, zlab=zlab, main=main)
+}
